6d258c7992f0a2930334399764785954d646ac8b
Lucas Barbosa <lucasbarbosa@Lucass-MacBook-Pro.local>
2021-01-06 22:09:17 -0300

    database connection fixed

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,3 +1,15 @@
-import { createConnection } from 'typeorm';
-
-createConnection();
+import { createConnection } from "typeorm";
+export const connectDb = async (retries = 10) => {
+  while (retries) {
+    try {
+      await createConnection();
+      break;
+    } catch (err) {
+      console.log(err);
+      retries -= 1;
+      console.log(`retries left: ${retries}`);
+      // wait 5 seconds
+      await new Promise(res => setTimeout(res, 5000));
+    }
+  }
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,11 +6,12 @@ import cors from 'cors';
 import routes from './routes';
 import errorHandler from './errors/handler';
 
-import './database/connection';
+import { connectDb } from './database/connection';
 
 const PORT = 3333;
 const HOST = '0.0.0.0'
 
+connectDb();
 const app = express();
 
 app.use(cors());
@@ -18,6 +19,5 @@ app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
-
 
 app.listen(PORT, HOST);
